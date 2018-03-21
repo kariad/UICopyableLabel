1cd1a6952ab6693f69fb47a86081a98857e01a6e
Daiki Katayama <karuiad.venezia@gmail.com>
2018-03-22 00:00:55 +0900

    Initial Commit

---
+++ UICopyableLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICopyableLabel
+//
+//  Created by Daiki Katayama on 2018/03/22.
+//  Copyright © 2018 Daiki Katayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
