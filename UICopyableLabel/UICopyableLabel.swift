668e8d330eb0ad3f9b416853fbc1bfaba9c7f6ae
Daiki Katayama <karuiad.venezia@gmail.com>
2018-03-22 01:08:28 +0900

    initial commit!

---
+++ UICopyableLabel/UICopyableLabel.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class UICopyableLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(showCopyMenu)))
+    }
+    
+    @objc func showCopyMenu() {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        if !menu.isMenuVisible {
+            menu.setTargetRect(bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return (action == #selector(copy(_:)))
+    }
+    
+    override func copy(_ sender: Any?) {
+        UIPasteboard.general.string = text
+        UIMenuController.shared.setMenuVisible(false, animated: true)
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+}
--- UICopyableLabel/ViewController.swift
+++ UICopyableLabel/ViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import PureLayout
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        view.backgroundColor = .red
+        
+        let copyableLabel = UICopyableLabel()
+        
+        view.addSubview(copyableLabel)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        copyableLabel.autoAlignAxis(toSuperviewAxis: .horizontal)
+        copyableLabel.autoAlignAxis(toSuperviewAxis: .vertical)
+        
+        copyableLabel.text = "test"
     }
 
 
